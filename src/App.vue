e28723e78229b9e7ffe5a4b0b9dd16c71780b3ee
Aslan Idrisov <idrisov.inmind@gmail.com>
2024-07-01 12:40:52 +0300

    First Commit
    First commit

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<script setup>
+import { computed, provide, ref, watch } from 'vue'
+
+import Header from './components/Header.vue'
+import Drawer from './components/Drawer.vue'
+
+
+/* Корзина (начало) */
+
+const cart = ref([])
+
+
+const drawerOpen = ref(false)
+
+
+const totalPrice = computed(() => cart.value.reduce((acc, item) => acc + item.price, 0))
+const vatPrice = computed(() => Math.round((totalPrice.value * 5) / 100))
+
+
+
+const closeDrawer = () => {
+  drawerOpen.value = false
+}
+
+const openDrawer = () => {
+  drawerOpen.value = true
+}
+
+const addToCart = (item) => {
+  item.isAdded = true
+  cart.value.push(item)
+}
+
+const removeFromCart = (item) => {
+  cart.value.splice(cart.value.indexOf(item), 1)
+  item.isAdded = false
+}
+
+
+watch(
+  cart,
+  () => {
+    localStorage.setItem('cart', JSON.stringify(cart.value))
+  },
+  { deep: true }
+)
+
+provide('cart', {
+  cart,
+  closeDrawer,
+  openDrawer,
+  removeFromCart,
+  addToCart
+})
+
+/* Корзина (конец) */
+
+</script>
+
+<template>
+  <Drawer
+    v-if="drawerOpen"
+    :total-price="totalPrice"
+    :vat-price="vatPrice"
+  />
+
+  <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
+    <Header @open-drawer="openDrawer" :total-price="totalPrice" />
+
+    <div class="p-10">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Card.vue
@@ -0,0 +1,38 @@
+<script setup>
+defineProps({
+  id: Number,
+  title: String,
+  imageUrl: String,
+  price: Number,
+  isFavorite: Boolean,
+  isAdded: Boolean,
+  onClickFavorite: Function,
+  onClickAdd: Function
+})
+</script>
+
+<template>
+  <div>
+    <div
+      class="relative flex flex-col w-full h-full border border-slate-100 rounded-xl p-8 cursor-pointer transition hover:shadow-xl hover:transform hover:-translate-y-2"
+    >
+      <img
+        v-if="onClickFavorite"
+        :src="!isFavorite ? '/like-1.svg' : '/like-2.svg'"
+        alt="Like 1"
+        class="absolute top-8 left-8"
+        @click="onClickFavorite"
+      />
+      <img :src="imageUrl" alt="Sneaker" />
+      <p class="h-full w-full">{{ title }}</p>
+
+      <div class="flex justify-between mt-5">
+        <div class="flex flex-col gap-2">
+          <span class="text-slate-400">Цена:</span>
+          <span class="font-bold">{{ price }} руб.</span>
+        </div>
+        <img v-if="onClickAdd" @click="onClickAdd" :src="!isAdded ? '/plus.svg' : '/checked.svg'" alt="Plus" />
+      </div>
+    </div>
+  </div>
+</template>
