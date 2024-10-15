ce2d18886142b966011617bd30d85b238e589ce3
Aslan Idrisov <112480803+Idrisovinm@users.noreply.github.com>
2024-10-15 14:36:40 +0300

    Update Favorites.vue

--- src/pages/Favorites.vue
+++ src/pages/Favorites.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import axios from 'axios'
-
 import CardList from '../components/CardList.vue'
 
 const favorites = ref([])
@@ -19,6 +18,5 @@ onMounted(async () => {
 
 <template>
   <h2 class="text-3xl font-bold mb-8">Мои закладки</h2>
-
   <CardList :items="favorites" is-favorites />
 </template>
